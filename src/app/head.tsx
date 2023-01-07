ba944157fe4f23b1c4f8cb6e2170327ed8d61280
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-01-08 00:45:31 +0900

    🩹

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,7 +5,7 @@ const Head: FC = () => {
     <>
       <title>Blog</title>
       <meta charSet="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </>
   );
