5a4de6b0a736f40b61cb1e0aa0102d5d13f67ee0
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-01-07 23:17:57 +0900

    ✅ add constant vars

---
+++ src/constant/index.ts
@@ -0,0 +1,21 @@
+const GITHUB_USERNAME = `mayone-du`;
+const BLOG_REPOSITORY_NAME = `blog-contents`;
+const MAIN_BRANCH = `main`;
+
+export const CONSTANTS = {
+  github: {
+    PROFILE_URL: `https://github.com/${GITHUB_USERNAME}`,
+    RAW_MARKDOWN_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/README.md`,
+    ARTICLE_LIST_META_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/meta/article_list.json`,
+    OTHER_LIST_META_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/meta/other_list.json`,
+    ARTICLE_BASE_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/articles/`,
+    PROFILE_MD_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/others/profile.md`,
+    RESUME_MD_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/resume/main/docs/Resume.md`,
+  },
+  zenn: {
+    PROFILE_URL: `https://zenn.dev/mayo__dev`,
+  },
+  twitter: {
+    PROFILE_URL: `https://twitter.com/mayo__dev`,
+  },
+} as const;
