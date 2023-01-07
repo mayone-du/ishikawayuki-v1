4d137c4c8f88d6776b84369c4eeba7ba1fbf680a
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-01-08 00:45:15 +0900

    🔧 important: true

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{tsx, ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-text": "#808B9F",
+        "main-bg": "#E5E5E5",
+        "neumorphism-bg": "#F0F0F3",
+        "neumorphism-active-bg": "#EEEEEE",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/typography"),
   ],
+  important: true,
 };
