6c03eb1d547602242be69f51d8ebcc0af36f4744
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-01-08 11:01:12 +0900

    🔨 image download script

--- script/main.ts
+++ script/main.ts
@@ -19,29 +19,54 @@ const getCodePoint = (emoji: string) => {
   return emoji.codePointAt(0)?.toString(16).toUpperCase();
 };
 
-const downloadMicrosoftTeamsEmoji = async (upperCaseEmojiName: string) => {
+const getFluentEmojiUrl = (upperCaseEmojiName: string) => {
+  const firstStrUpperSpaceCase =
+    upperCaseEmojiName.charAt(0).toUpperCase() +
+    upperCaseEmojiName.slice(1).toLowerCase();
+  const lowerSnakeCase = upperCaseEmojiName.toLowerCase().replaceAll(" ", "_");
+
+  const url = `https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/${encodeURI(
+    firstStrUpperSpaceCase
+  )}/3D/${lowerSnakeCase}_3d.png`;
+  return url;
+};
+
+const getMicrosoftTeamsEmojiUrl = async (upperCaseEmojiName: string) => {
   const lowerHyphenCase = upperCaseEmojiName.toLowerCase().replaceAll(" ", "-");
   const url = `https://emojipedia.org/microsoft-teams/1.0/${lowerHyphenCase}`;
   const res = await fetch(url);
   const html = await res.text();
   const $ = load(html);
   const imageUrl = $(".vendor-set-emoji-image").children("img").attr("src");
-  if (!imageUrl) return;
-  const imageRes = await (await fetch(imageUrl)).arrayBuffer();
-  const savePath = `public/assets/${lowerHyphenCase}.png`;
+  return imageUrl;
+};
+
+const downloadFromImageUrl = async (url: string, name: string) => {
+  const imageRes = await (await fetch(url)).arrayBuffer();
+  const savePath = `public/assets/${name}.png`;
   fs.writeFileSync(savePath, Buffer.from(imageRes), "binary");
 };
 
 const main = async () => {
   const articleList = await fetchArticleList();
-  articleList.forEach(async ({ emoji }) => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  articleList.forEach(async ({ emoji, is_published }) => {
+    if (!is_published) return;
     const codePoint = getCodePoint(emoji);
     const emojiDataSourceRes = await fetch(CONSTANTS.emoji.DATA_SOURCE);
     const emojiDataSource =
       (await emojiDataSourceRes.json()) as EmojiDataSource;
     const hit = emojiDataSource.find((item) => item.unified === codePoint);
     if (!hit) return;
-    await downloadMicrosoftTeamsEmoji(hit.name);
+    const lowerSnakeCase = hit.name.toLowerCase().replaceAll(" ", "_");
+    try {
+      let imageUrl = await getMicrosoftTeamsEmojiUrl(hit.name);
+      if (!imageUrl) imageUrl = getFluentEmojiUrl(hit.name);
+      if (!imageUrl) return;
+      downloadFromImageUrl(imageUrl, lowerSnakeCase);
+    } catch (error) {
+      console.error(error);
+    }
   });
 };
 
