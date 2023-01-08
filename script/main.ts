ec05ff907a4b1cb18528845ff345b6a0d25652fd
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-01-08 17:11:12 +0900

    🩹 未完成

--- script/main.ts
+++ script/main.ts
@@ -5,6 +5,10 @@ import { load } from "cheerio";
 import { CONSTANTS } from "../src/constant";
 import type { ArticleList, EmojiDataSource } from "../src/types";
 
+// 画像ダウンロードしたときに、404の場合は14byteくらいのレスポンスが返るため、判定用の閾値を設けてretryする
+const BYTE_LENGTH_THRESHOLD = 100;
+const RETRY_ERROR_MESSAGE = "RETRY";
+
 const fetchArticleList = async () => {
   try {
     const res = await fetch(CONSTANTS.github.ARTICLE_LIST_META_URL);
@@ -19,7 +23,10 @@ const getCodePoint = (emoji: string) => {
   return emoji.codePointAt(0)?.toString(16).toUpperCase();
 };
 
-const getFluentEmojiUrl = (upperCaseEmojiName: string) => {
+const getFluentEmojiUrl = (
+  upperCaseEmojiName: string,
+  isAddDefaultPath?: boolean // TODO: emojiによってパスが変わる。https://github.com/microsoft/fluentui-emoji を要確認
+) => {
   const firstStrUpperSpaceCase =
     upperCaseEmojiName.charAt(0).toUpperCase() +
     upperCaseEmojiName.slice(1).toLowerCase();
@@ -27,7 +34,7 @@ const getFluentEmojiUrl = (upperCaseEmojiName: string) => {
 
   const url = `https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/${encodeURI(
     firstStrUpperSpaceCase
-  )}/3D/${lowerSnakeCase}_3d.png`;
+  )}${isAddDefaultPath ? "/Default" : ""}/3D/${lowerSnakeCase}_3d.png`;
   return url;
 };
 
@@ -43,6 +50,8 @@ const getMicrosoftTeamsEmojiUrl = async (upperCaseEmojiName: string) => {
 
 const downloadFromImageUrl = async (url: string, name: string) => {
   const imageRes = await (await fetch(url)).arrayBuffer();
+  if (imageRes.byteLength < BYTE_LENGTH_THRESHOLD) return RETRY_ERROR_MESSAGE;
+
   const savePath = `public/assets/${name}.png`;
   fs.writeFileSync(savePath, Buffer.from(imageRes), "binary");
 };
@@ -58,12 +67,17 @@ const main = async () => {
       (await emojiDataSourceRes.json()) as EmojiDataSource;
     const hit = emojiDataSource.find((item) => item.unified === codePoint);
     if (!hit) return;
+
     const lowerSnakeCase = hit.name.toLowerCase().replaceAll(" ", "_");
     try {
       let imageUrl = await getMicrosoftTeamsEmojiUrl(hit.name);
       if (!imageUrl) imageUrl = getFluentEmojiUrl(hit.name);
       if (!imageUrl) return;
-      downloadFromImageUrl(imageUrl, lowerSnakeCase);
+      const result = await downloadFromImageUrl(imageUrl, lowerSnakeCase);
+      if (result === RETRY_ERROR_MESSAGE) {
+        imageUrl = getFluentEmojiUrl(hit.name, true);
+        await downloadFromImageUrl(imageUrl, lowerSnakeCase);
+      }
     } catch (error) {
       console.error(error);
     }
