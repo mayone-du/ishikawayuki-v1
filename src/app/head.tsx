74fb1ff3e511b067fdc5c3cc75963c62ba8e5ce4
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-01-08 20:10:18 +0900

    💄 favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,12 +1,18 @@
 import type { FC } from "react";
+import { staticPath } from "src/lib/$path";
 
 const Head: FC = () => {
   return (
     <>
-      <title>Blog</title>
+      <title>Ishikawa Yuki</title>
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="shortcut icon"
+        href={staticPath.assets.dog_png}
+        type="image/x-icon"
+      />
     </>
   );
 };
--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,30 +1,46 @@
 export const pagesPath = {
-  "article": {
+  article: {
     _articleId: (articleId: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/article/[articleId]' as const, query: { articleId }, hash: url?.hash })
+      $url: (url?: { hash?: string }) => ({
+        pathname: "/article/[articleId]" as const,
+        query: { articleId },
+        hash: url?.hash,
+      }),
     }),
-    $url: (url?: { hash?: string }) => ({ pathname: '/article' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/article" as const,
+      hash: url?.hash,
+    }),
   },
-  "diary": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/diary' as const, hash: url?.hash })
+  diary: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/diary" as const,
+      hash: url?.hash,
+    }),
   },
-  "about": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
+  about: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/about" as const,
+      hash: url?.hash,
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
-}
+  $url: (url?: { hash?: string }) => ({
+    pathname: "/" as const,
+    hash: url?.hash,
+  }),
+};
 
-export type PagesPath = typeof pagesPath
+export type PagesPath = typeof pagesPath;
 
 export const staticPath = {
   assets: {
-    $1st_place_medal_png: '/assets/1st_place_medal.png',
-    building_construction_png: '/assets/building_construction.png',
-    clinking_beer_mugs_png: '/assets/clinking_beer_mugs.png',
-    tiger_face_png: '/assets/tiger_face.png'
+    $1st_place_medal_png: "/assets/1st_place_medal.png",
+    building_construction_png: "/assets/building_construction.png",
+    clinking_beer_mugs_png: "/assets/clinking_beer_mugs.png",
+    dog_png: "/assets/dog.png",
+    tiger_face_png: "/assets/tiger_face.png",
   },
-  favicon_ico: '/favicon.ico',
-  profile_jpg: '/profile.jpg'
-} as const
+  profile_jpg: "/profile.jpg",
+} as const;
 
-export type StaticPath = typeof staticPath
+export type StaticPath = typeof staticPath;
