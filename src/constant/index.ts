5031c2bc679f941a8581a56b4cde78792dd6a3a2
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-11-21 02:43:11 +0900

    tweak

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,14 +2,16 @@ const GITHUB_USERNAME = `mayone-du`;
 const BLOG_REPOSITORY_NAME = `blog-contents`;
 const MAIN_BRANCH = `main`;
 
+const RAW_URL = `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}`;
+
 export const CONSTANTS = {
   github: {
     PROFILE_URL: `https://github.com/${GITHUB_USERNAME}`,
-    RAW_MARKDOWN_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/README.md`,
-    ARTICLE_LIST_META_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/meta/article_list.json`,
-    OTHER_LIST_META_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/meta/other_list.json`,
-    ARTICLE_BASE_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/articles/`,
-    PROFILE_MD_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${BLOG_REPOSITORY_NAME}/${MAIN_BRANCH}/others/profile.md`,
+    RAW_MARKDOWN_URL: `${RAW_URL}/README.md`,
+    ARTICLE_LIST_META_URL: `${RAW_URL}/meta/article_list.json`,
+    OTHER_LIST_META_URL: `${RAW_URL}/meta/other_list.json`,
+    ARTICLE_BASE_URL: `${RAW_URL}/articles/`,
+    PROFILE_MD_URL: `${RAW_URL}/others/profile.md`,
     RESUME_MD_URL: `https://raw.githubusercontent.com/${GITHUB_USERNAME}/resume/main/docs/Resume.md`,
   },
   zenn: {
@@ -20,7 +22,7 @@ export const CONSTANTS = {
   },
   emoji: {
     DATA_SOURCE:
-      "https://cdn.jsdelivr.net/npm/emoji-datasource@14.0.0/emoji.json",
+      "https://cdn.jsdelivr.net/npm/emoji-datasource@latest/emoji.json",
   },
   origin: {
     HOST:
