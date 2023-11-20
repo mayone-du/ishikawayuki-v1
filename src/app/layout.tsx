41099259f29e2c52bf7694646a56ad9337672126
mayone-du <yuki.ishikawa.dev@gmail.com>
2023-11-21 03:04:04 +0900

    fix head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,20 @@ import { Rubik } from "next/font/google";
 const rubik = Rubik({ subsets: [] });
 
 import type { FC, PropsWithChildren } from "react";
+import { staticPath } from "src/lib/$path";
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="ja">
+      <title>Ishikawa Yuki</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="shortcut icon"
+        href={staticPath.assets.dog_png}
+        type="image/x-icon"
+      />
       <body className={`bg-main-bg text-main-text ${rubik.style.fontFamily}`}>
         {children}
 
