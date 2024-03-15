0d1e21a785da6431215226597dfdfa8e23550f08
mayone-du <yuki.ishikawa.dev@gmail.com>
2024-03-16 04:02:39 +0900

    💡

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 
+// TODO:
 export const Pagination: FC = () => {
   return (
     <div className="h-10 neumorphism-container-preset neumorphism-container-md mt-10 mx-auto max-w-[200px]">
